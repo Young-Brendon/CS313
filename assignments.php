36d0437a1c60c480e66f980ffa26203bb8ba20bf
unknown <ybrendon@hotmail.com>
2015-04-30 13:18:02 -0700

    welcome page

---
+++ assignments.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Brendon Young - CS 313</title>
+		<link href="/users/bdawg/php/style.css" type="text/css" rel="stylesheet" media="screen" >
+	</head>
+	<body>
+		<div id="header">
+			<div id='banner'>
+				<h1>Brendon Young - CS 313</h1>
+			</div>
+			<nav id='nav'>
+				<ul id="navli">
+					<li><a href="/users/bdawg/php/index.php" title="Home">Home</a></li>
+					<li><a href="/users/bdawg/php/assignments.php" title="Assignments">Assignments</a></li>
+				</ul>
+			</nav>
+		</div>
+		<p>Under construction...</p>
+		<footer id="footer">
+			<p>&copy; - Brendon Young 2015</p>
+		</footer>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!doctype html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to OpenShift</title>
-
-</head>
-<body>
-
-<h1>Hello World!</h1>
-
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Brendon Young - CS 313</title>
+		<link href="/users/bdawg/php/style.css" type="text/css" rel="stylesheet" media="screen" >
+	</head>
+	<body>
+		<div id="header">
+			<div id='banner'>
+				<h1>Brendon Young - CS 313</h1>
+			</div>
+			<nav id='nav'>
+				<ul id="navli">
+					<li><a href="/users/bdawg/php/index.php" title="Home">Home</a></li>
+					<li><a href="/users/bdawg/php/assignments.php" title="Assignments">Assignments</a></li>
+				</ul>
+			</nav>
+		</div>
+		<h2>Home Page</h2>
+		<p>My name is Brendon Young and I have a wonderful wife and son to enjoy life 
+		with. I am currently attending Brigham Young University - Idaho as an online 
+		student working on a BA in Web Design and Development.  I enjoy working with 
+		computers and building websites, but more so working with web businesses and
+		SEO (Search Engine Optimization).  Programming and design is enjoyable for me.
+		In my spare time I enjoy family outings, playing League of Legends and reading.  
+		I consider myself hard working and dedicated to any task that needs completed.  
+		This website is where all of the assignments for CS 313 will be found.</p>
+		<img src="/users/bdawg/php/brendon-young.jpg" alt="Picture of Brendon Young">
+		<img src="/users/bdawg/php/my-family.jpg" alt="My family and I in Oregon">
+		<footer id="footer">
+			<p>&copy; - Brendon Young 2015</p>
+		</footer>
+	</body>
 </html>
