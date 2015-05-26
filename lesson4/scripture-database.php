695a7e1fc860ac498b4b23e45fa7b17d6e94bb65
unknown <ybrendon@hotmail.com>
2015-05-26 15:11:58 -0700

    Scripture Database

--- lesson4/scripture-database.php
+++ lesson4/scripture-database.php
@@ -1,8 +1,59 @@
-<?php 
-	require '/dbconnection.php';
+<?php
+	
+	function loadDatabase() {
+
+		
+		$dbName = "mydb";		
+        $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        $dbUser = 'test';
+        $dbPassword = 'test';
+		  		  
+		echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";		 
+     
+		$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		return $db;
+
+	}
+	
+	function getScriptureItems() {      
+		
+		$query = "SELECT book, chapter, verse, content FROM scriptures ORDER BY id";
+		return DbSelect($query);
+    }
+
+	function DbSelect($query, $params = null) {
+	
+		$db = "";
+        $return = array();        
+
+        try {
+
+            $statement = $db->prepare($query);
+			if (is_array($params)) {
+                foreach ($params as $key => $value) {
+                    $statement->bindValue($key, $value);
+                }
+            }
+            $statement->Execute();
+            while ($row = $statement->fetch()) {
+                $return[] = $row;
+            }
+        }
+        catch (Exception $e) {
+            echo "Database exception, try again later.";
+            error_log($e);
+            exit();
+        }
+        return $return;
+    }
 	$db = loadDatabase();
 	$items = getScriptureItems();
 ?>
+	
+	
+
 
 <!DOCTYPE html>
 <html lang="en">
