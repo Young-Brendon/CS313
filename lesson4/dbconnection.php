bd765f4d83e718e0eb7f98aa67f523a22994afcb
unknown <ybrendon@hotmail.com>
2015-05-26 14:56:22 -0700

    Scripture Database

--- lesson4/dbconnection.php
+++ lesson4/dbconnection.php
@@ -8,7 +8,7 @@
 		$dbPort = "";
 		$dbUser = "";
 		$dbPassword = "";
-		$dbName = "mydb"
+		$dbName = "mydb";
 		$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
 		if ($openShiftVar === null || $openShiftVar == "") {
@@ -23,8 +23,8 @@
 		  echo "Using openshift credentials: ";
           $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 		  $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-          $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-          $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+          $dbUser = 'test';
+          $dbPassword = 'test';
 		  		  
 		  echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 		} 
