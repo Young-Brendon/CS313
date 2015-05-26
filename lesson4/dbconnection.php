c3cc31b7163abf40d55443f7214d441bb6d2c4d0
unknown <ybrendon@hotmail.com>
2015-05-26 15:07:18 -0700

    Scripture Database

--- lesson4/dbconnection.php
+++ lesson4/dbconnection.php
@@ -2,31 +2,15 @@
 	
 	function loadDatabase() {
 
-		$db = "";
-		$dbHost = "";
-		$dbPort = "";
-		$dbUser = "";
-		$dbPassword = "";
-		$dbName = "mydb";
-		$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
-
-		if ($openShiftVar === null || $openShiftVar == "") {
-			// Not in the openshift environment
-			$dbHost = "localhost";
-			$dbUser = "root";
-			$dbPassword = "";			
-			
-		} else { 
 		
-          // In the openshift environment		  
-		  echo "Using openshift credentials: ";
-          $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-		  $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-          $dbUser = 'test';
-          $dbPassword = 'test';
+		$dbName = "mydb";
+		
+        $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+        $dbUser = 'test';
+        $dbPassword = 'test';
 		  		  
-		  echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
-		} 
+		echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";		 
      
 		$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
