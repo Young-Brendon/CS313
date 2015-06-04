d4131b9d1e4ce49fae646ba8317ffb277075f8d6
unknown <ybrendon@hotmail.com>
2015-06-04 11:40:00 -0700

    Gallery Database

--- gallery.php
+++ gallery.php
@@ -1,7 +1,7 @@
 <?php
 	error_reporting(E_ALL);
     ini_set("display_errors", 1);
-	include 'dbconnection.php';
+	include 'lesson4/dbconnection.php';
 	$db = loadDatabase();
 	$items = getGalleryImages();	
 ?>
