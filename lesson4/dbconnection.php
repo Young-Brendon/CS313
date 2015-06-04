c155abb5b8ecc33966b0ffd9d5e3585c397c5ffd
unknown <ybrendon@hotmail.com>
2015-06-03 18:44:34 -0700

    Scripture Database

--- lesson4/dbconnection.php
+++ lesson4/dbconnection.php
@@ -3,12 +3,14 @@
 	global $db;
 	function loadDatabase() {	
 		
-        $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
 		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-		
+		$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+		$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+        		
         $dbName = "mydb";				 
      
-		$db = new PDO("mysql:host=$dbHost;dbname=$dbName", "test", "test");
+		$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
 		return $db;
 
