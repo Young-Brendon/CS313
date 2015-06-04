83bd5f6dee0f05c732b8bfbc3b7c91f314bbae15
unknown <ybrendon@hotmail.com>
2015-06-04 12:37:30 -0700

    Gallery Database

--- gallery.php
+++ gallery.php
@@ -30,11 +30,15 @@
 		<p>Feel free to post a picture of nature that you enjoy and/or comment on any picture.  Please only upload family appropriate
 		images and make pg rated comments.  Any violations will be removed.</p>
 		<?php foreach($items as $item) : ?>
-		<h2><?php echo $item['title']; ?></h2><br>
-		<img src="<?php echo $item['image']; ?>" alt="<?php echo $item['title']; ?>" /><br>
-		Caption: <?php echo $item['caption']; ?><br><br>
-		<div id="comments">Comments: <?php echo $item['comments']; ?><br><br>
-		<?php echo $item['date']; ?></div><br><br>
+		<div id="galleryLayout">
+			<h2><?php echo $item['title']; ?></h2><br>
+			<img src="<?php echo $item['image']; ?>" alt="<?php echo $item['title']; ?>" /><br>
+			Caption: <?php echo $item['caption']; ?><br><br>
+			<div id="comments">
+				Comments: <?php echo $item['comments']; ?><br><br>
+				<?php echo $item['date']; ?>
+			</div><br><br>
+		</div>
 		<?php endforeach;?>	
 		<footer id="footer">
 			<p>&copy; - Brendon Young 2015</p>
