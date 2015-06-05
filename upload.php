32fe1ddf2021f11b4e1e5396a75652c72a79fa0e
unknown <ybrendon@hotmail.com>
2015-06-04 17:42:18 -0700

    Gallery Database

--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 
 <h2>Upload a Picture</h2>
 <p>Use the form to upload a picture.  All fields are required.
-<form action="results.php" method="post">
+<form id="uploadForm" action="results.php" method="post">
 	Title: <input type="text" name="title"><br><br>
-	Image to be uploaded:<br><br>
+	Image to be uploaded: 
 	<input type="hidden" name="action" value="upload">
-	<input type="file" name="file1">
+	<input type="file" name="file1"><br><br>
 	Caption: <input type="text" name="caption">
 	<input type="submit">
 </form>
